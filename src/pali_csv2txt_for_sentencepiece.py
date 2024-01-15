c68a4bff68d5b59a1c4fc77ab433a95932bfa847
Vladimir Angirov <angirov@posteo.net>
2024-01-15 13:15:47 +0100

    add html filter to pali cleaner

--- src/pali_csv2txt_for_sentencepiece.py
+++ src/pali_csv2txt_for_sentencepiece.py
@@ -10,6 +10,10 @@ dest_file = Path("/home/wo/bn/paltok/pali_for_sentencepiece.txt")
 
 def clean_pali(string):
         string = string.lower()
+
+        html_tags = re.compile('<.*?>')
+        string = re.sub(html_tags, "", string)
+
         string = re.sub(r'[0-9!"#$%&()*+,-./:;<=>?@[\]^_`{|}~\']',"", string) # ascii digits and punctuation
         string = re.sub(r'[\t\n\r\x0b\x0c]'," ", string) # whitespaces apart from " "
         string = re.sub(r'[ṅṁ]',"ṃ", string) # whitespaces apart from " "
