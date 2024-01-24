b884385c6a01c2779fde6971adf4012e5b1f6a80
Vladimir Angirov <angirov@posteo.net>
2024-01-24 21:58:28 +0100

    add sentence as py module

--- src/tasks.py
+++ src/tasks.py
@@ -3,9 +3,10 @@ from pali_json2tsv import pali_repo2tsv
 from pali_prep_spm import pali_prep_spm
 import os
 from pathlib import Path
-
+from tqdm import tqdm
 from timestamp import stamp2datetime, get_timestapm, find_latest
 from pali_prep_spm import PALI_FOR_SPM_FILENAME
+import sentencepiece as spm
 
 # invoke create-pali-all-tsv --json-dir "../segmented-pali/inputfiles/" --tsv-dir "../pali_all"
 @task
@@ -55,3 +56,20 @@ def print_timestapm(c, path):
 def print_txt_for_spm(c, path):
     print(find_latest(path, PALI_FOR_SPM_FILENAME))
     return 0
+
+@task
+def train_spm(c, input, model_prefix, vocab_size=4000):
+    spm.SentencePieceTrainer.train(input=input, 
+                                    model_prefix=model_prefix, 
+                                    vocab_size=vocab_size, 
+                                    user_defined_symbols=['foo', 'bar'])
+    
+@task
+def encode_for_fasttext(c, input_path, model_path, output_path):
+    sp = spm.SentencePieceProcessor()
+    sp.load(model_path)
+    with open(input_path, "r") as in_file:
+        with open(output_path, "w+") as out_file:
+            for line in tqdm(in_file):
+                tokens = sp.encode_as_pieces(line)
+                out_file.write(" ".join(tokens) + "\n")
--- src/train_models.sh
+++ src/train_models.sh
@@ -4,7 +4,7 @@ OUTPUT_DIR=$(realpath ${WORK_DIR}/../output) #"/output/"
 TASKS=$(realpath ${OUTPUT_DIR}/src)
 SRC_DIR=${OUTPUT_DIR}/src
 
-FASTTEXT_BINARY=${WORK_DIR}/../../fastText/fasttext
+FASTTEXT_BINARY=${WORK_DIR}/../fastText/fasttext
 
 mkdir -p ${OUTPUT_DIR}
 
@@ -23,13 +23,21 @@ SUFFIX=$(invoke print-timestapm --path ${TXT_FOR_SPM})
 
 TOKENIZED=${OUTPUT_DIR}/pali_tokenized_for_fasttext${SUFFIX}.txt
 
-time spm_train --input=${OUTPUT_DIR}/${TXT_FOR_SPM} \
-    --model_prefix=${OUTPUT_DIR}/pali_spm${SUFFIX} \
-    --vocab_size=4000 --character_coverage=1.0
+# time spm_train --input=${OUTPUT_DIR}/${TXT_FOR_SPM} \
+#     --model_prefix=${OUTPUT_DIR}/pali_spm${SUFFIX} \
+#     --vocab_size=4000 --character_coverage=1.0
 
-echo Tokenizing the corpus for FastText
-time cat ${OUTPUT_DIR}/${TXT_FOR_SPM} | spm_encode \
-    --model=${OUTPUT_DIR}/pali_spm${SUFFIX}.model > ${TOKENIZED}
+invoke train-spm    --input=${OUTPUT_DIR}/${TXT_FOR_SPM} \
+                    --model-prefix=${OUTPUT_DIR}/pali_spm${SUFFIX} \
+                    --vocab-size=4000 
+
+# echo Tokenizing the corpus for FastText
+# time cat ${OUTPUT_DIR}/${TXT_FOR_SPM} | spm_encode \
+#     --model=${OUTPUT_DIR}/pali_spm${SUFFIX}.model > ${TOKENIZED}
+
+invoke encode-for-fasttext  --input-path=${OUTPUT_DIR}/${TXT_FOR_SPM} \
+                            --model-path=${OUTPUT_DIR}/pali_spm${SUFFIX}.model \
+                            --output-path=${TOKENIZED}
 
 echo Training a FastText model
 time ${FASTTEXT_BINARY} skipgram \
