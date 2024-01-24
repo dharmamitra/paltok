0b82b120fe1aaa47ac6811321b29c848bdb67287
Vladimir Angirov <angirov@posteo.net>
2024-01-24 19:51:56 +0100

    general script from JSON to training FastText

--- src/train_models.sh
+++ src/train_models.sh
@@ -1,37 +1,34 @@
 WORK_DIR=$(pwd)
-OUTPUT_DIR=$(realpath ${WORK_DIR}/..) #"/output/"
+JSON_DIR=$(realpath ${WORK_DIR}/../segmented-pali/inputfiles/)
+OUTPUT_DIR=$(realpath ${WORK_DIR}/../output) #"/output/"
 TASKS=$(realpath ${OUTPUT_DIR}/src)
 SRC_DIR=${OUTPUT_DIR}/src
 
 FASTTEXT_BINARY=${WORK_DIR}/../../fastText/fasttext
 
-
 mkdir -p ${OUTPUT_DIR}
 
-cd ${SRC_DIR}
-
-# invoke create-pali-all-tsv \
-#     --json-dir ${OUTPUT_DIR}"/segmented-pali/inputfiles/" \
-#     --tsv-dir ${OUTPUT_DIR}
-
+invoke create-pali-all-tsv \
+    --json-dir ${JSON_DIR} \
+    --tsv-dir ${OUTPUT_DIR} && \
 invoke prep-spm --src-dir=${OUTPUT_DIR}
 
-INPUT=$1
-if [ -z $INPUT ]; then
-    INPUT=$(invoke print-txt-for-spm ${OUTPUT_DIR})
+# the result of the previous step is previous step is found by the latest timestamp
+
+TXT_FOR_SPM=$1
+if [ -z ${TXT_FOR_SPM} ]; then
+    TXT_FOR_SPM=$(invoke print-txt-for-spm ${OUTPUT_DIR})
 fi
-echo INPUT = $INPUT
-SUFFIX=$(invoke print-timestapm --path $INPUT)
-echo $SUFFIX
-TXT_FOR_SPM=${OUTPUT_DIR}/$INPUT
+SUFFIX=$(invoke print-timestapm --path ${TXT_FOR_SPM})
+
 TOKENIZED=${OUTPUT_DIR}/pali_tokenized_for_fasttext${SUFFIX}.txt
 
-time spm_train --input=${TXT_FOR_SPM} \
+time spm_train --input=${OUTPUT_DIR}/${TXT_FOR_SPM} \
     --model_prefix=${OUTPUT_DIR}/pali_spm${SUFFIX} \
     --vocab_size=4000 --character_coverage=1.0
 
 echo Tokenizing the corpus for FastText
-time cat ${TXT_FOR_SPM} | spm_encode \
+time cat ${OUTPUT_DIR}/${TXT_FOR_SPM} | spm_encode \
     --model=${OUTPUT_DIR}/pali_spm${SUFFIX}.model > ${TOKENIZED}
 
 echo Training a FastText model
